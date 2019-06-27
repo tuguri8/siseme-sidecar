7ea5e2f72a56d3f38e9df0b42425f1f31a298ca2
한승우 <tuguri8@gmail.com>
2019-06-28 02:19:37 +0900

    #1 코드 리팩토링(피드백 반영)

--- shorturl-backend/src/main/java/me/sise/sidecar/shorturl/service/ShortUrlServiceImpl.java
+++ shorturl-backend/src/main/java/me/sise/sidecar/shorturl/service/ShortUrlServiceImpl.java
@@ -9,8 +9,10 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import java.time.format.DateTimeFormatter;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -45,14 +47,22 @@ public class ShortUrlServiceImpl implements ShortUrlService {
 
     @Override
     public String getRouteUrl(String path) {
-        ShortUrl shortUrl = shortUrlRepository.findByPathAndIsActive(path, true);
+        ShortUrl shortUrl = shortUrlRepository.findByPathAndIsActive(path, Boolean.TRUE);
         return Objects.isNull(shortUrl) ? MAIN_URL : shortUrl.getWebUrl();
     }
 
     @Override
     public ShortUrlModel updateShortUrl(String path, String customPath, String webUrl) {
-        String newPath = StringUtils.isEmpty(customPath) ? path : customPath;
-        String newWebUrl = StringUtils.isEmpty(webUrl) ? getRouteUrl(path) : webUrl;
+
+//        String newPath = StringUtils.isEmpty(customPath) ? path : customPath;
+//        String newWebUrl = StringUtils.isEmpty(webUrl) ? getRouteUrl(path) : webUrl;
+
+        // Optional으로 리팩토링 ( ""에 대한 예외처리가 안됨 )
+        String newPath = Optional.ofNullable(customPath).orElse(path);
+        String newWebUrl = Optional.ofNullable(webUrl).orElse(getRouteUrl(path));
+
+
+
         if (!existsPath(path)) {
             throw new UrlShorteningFailureException.PathIsNotExist();
         }
@@ -63,7 +73,7 @@ public class ShortUrlServiceImpl implements ShortUrlService {
             throw new UrlShorteningFailureException.CustomPathIsAlreadyInUseException();
         }
 
-        ShortUrl shortUrl = shortUrlRepository.findByPathAndIsActive(path, true);
+        ShortUrl shortUrl = shortUrlRepository.findByPathAndIsActive(path, Boolean.TRUE);
         shortUrl.setPath(newPath);
         shortUrl.setWebUrl(newWebUrl);
         return transform(shortUrlRepository.save(shortUrl));
@@ -74,7 +84,7 @@ public class ShortUrlServiceImpl implements ShortUrlService {
         if (!existsPath(path)) {
             throw new UrlShorteningFailureException.PathIsNotExist();
         }
-        ShortUrl shortUrl = shortUrlRepository.findByPathAndIsActive(path, true);
+        ShortUrl shortUrl = shortUrlRepository.findByPathAndIsActive(path, Boolean.TRUE);
         shortUrl.setIsActive(Boolean.FALSE);
         return transform(shortUrlRepository.save(shortUrl));
     }
@@ -84,13 +94,13 @@ public class ShortUrlServiceImpl implements ShortUrlService {
         if (!existsPath(path)) {
             throw new UrlShorteningFailureException.PathIsNotExist();
         }
-        return transform(shortUrlRepository.findByPathAndIsActive(path, true));
+        return transform(shortUrlRepository.findByPathAndIsActive(path, Boolean.TRUE));
     }
 
     @Override
     public List<ShortUrlModel> showAllShortUrl() {
-        shortUrlRepository.findAllByIsActive(true).orElseThrow(() -> new UrlShorteningFailureException.PathIsNotExist());
-        return shortUrlRepository.findAllByIsActive(true).get().stream().map(x -> transform(x)).collect(Collectors.toList());
+        if (!shortUrlRepository.findAllByIsActive(Boolean.TRUE).isPresent()) {return Collections.EMPTY_LIST;}
+        return shortUrlRepository.findAllByIsActive(Boolean.TRUE).get().stream().map(x -> transform(x)).collect(Collectors.toList());
     }
 
     private Boolean existsPath(String path) {
